f0692badd8306a9dd28340facc1d2a1f7a2d0765
Noel Felix <nn.felix.25@gmail.com>
2026-02-16 20:32:38 -0800

    Exercise 1

--- exercises/1.c
+++ exercises/1.c
@@ -1,4 +1,4 @@
-// #include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 // 👉 First, build and run the program.
@@ -12,18 +12,19 @@ int main() {
     //    changing the length to 14.
     // 👉 Try making the length less than 14. What happens when you run the program?
     // 👉 Try making the length much longer than 14, like 200. What happens? 😱
-    write(1, "Hello, World!", 13);
+    write(1, "Hello, World!\n", 14);
 
     // 👉 Try uncommenting the next 2 lines, as well as the #include <stdio.h> at the top.
     //    (You'll want to change the length of write() above back to 14 first!)
     // 👉 Try adding a second number, named num2. Set it to something other than 42.
-    // int num = 42;
-    // printf("The number is: %d\n", num);
+    int num = 42;
+    int num2 = 67;
+    printf("The number is: %d\t%d\n", num, num2);
 
     // 👉 Try having printf print *both* numbers.
     //    Hint: you'll need to make 2 changes to printf's arguments to do this!
 
     // 👉 Try returning something other than 0. (To see it, you'll need to run `echo $?`
     //    immediately after the program finishes running.)
-    return 0;
+    return 4;
 }
---
+++ exercises/1.s
@@ -0,0 +1,30 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 15, 0	sdk_version 26, 2
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$42, -8(%rbp)
+	movl	-8(%rbp), %esi
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	movl	$4, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"The number is: %d\t"
+
+.subsections_via_symbols
